965cf2cb6d5b8afc10a98022a6abd131178615bd
_crhis_ <crhisnokia@live.com>
2023-05-03 09:46:44 -0300

    refactor: adicionando funcao de criar elemento div

--- 1-SEMESTRE/DESENV-WEB-1/JAVASCRIPT/exercicio-2/script.js
+++ 1-SEMESTRE/DESENV-WEB-1/JAVASCRIPT/exercicio-2/script.js
@@ -1,10 +1,24 @@
-function exibir (){
+function exibir() {
 
-let image_input = document.querySelector("input#image");
+    let image_input = document.querySelector("input#image");
 
-let image = document.querySelector("div > img");
+    let image = document.querySelector("div > img");
 
-image.src = `${image_input.value}`
-
+    image.src = `${image_input.value}`
 
 }
+
+
+/* function exibir() {
+
+    let image_input = document.querySelector("input#image");
+
+    let image = document.createElement("div");
+    image.innerHTML = `
+    <img src= ${image_input.value}/>
+    `
+    const newDiv = document.querySelector("div.container");
+
+    newDiv.appendChild(image)
+
+} */
