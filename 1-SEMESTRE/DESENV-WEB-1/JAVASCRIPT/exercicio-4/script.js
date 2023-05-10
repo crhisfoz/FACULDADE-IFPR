bfc0a98691d31ba2dd4c565a158c6ab465cdc0a0
_crhis_ <crhisnokia@live.com>
2023-05-10 14:01:58 -0300

    refactor: refatoracao exercicio 4 para melhor perfomance com switch case

--- 1-SEMESTRE/DESENV-WEB-1/JAVASCRIPT/exercicio-4/script.js
+++ 1-SEMESTRE/DESENV-WEB-1/JAVASCRIPT/exercicio-4/script.js
@@ -1,39 +1,23 @@
 function calcular() {
-
     let input = document.querySelector("input#valor").value.toUpperCase();
-
     let note = document.querySelector("input#nota");
-
-    if(input === "A"){
+  
+    switch (input) {
+      case 'A':
         note.value = 10;
-    }
-    else if(input === "B"){
+        break;
+      case 'B':
         note.value = 8;
-    }
-    else if(input === "C"){
+        break;
+      case 'C':
         note.value = 6;
-    }
-    else if(input === "D"){
+        break;
+      case 'D':
         note.value = 3;
+        break;
+      default:
+        alert("Valor inválido");
+        break;
     }
-    else{
-        alert("valor inválido")
-    }
-   /*  switch (input) {
-        case input === "A":
-            note.value = 10
-            break;
-        case input === "B":
-            note.value = 8
-            break;
-        case input === "C":
-            note.value = 6
-            break;
-        case input.value == "D":
-            note.value = 3
-            break;
-        default:
-            alert("valor inválido")
-            break;
-    } */
-}
+  }
+  
