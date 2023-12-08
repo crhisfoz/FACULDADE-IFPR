cd8bbf73d48c5df4960d3188c963d89ea2d44b29
crhisfoz <crhisnokia@live.com>
2023-12-08 20:37:57 -0300

    refactor: reajuste de metodos para reajustar o retorno dos veiculos do banco de dados para um Json, e adicionada mensagem de retorno de sucesso na funcao deletar

--- 2-SEMESTRE/DESENV-WEB-2/exercicio-api-rest/api-veiculos/app/Controller/VeiculoController.php
+++ 2-SEMESTRE/DESENV-WEB-2/exercicio-api-rest/api-veiculos/app/Controller/VeiculoController.php
@@ -12,94 +12,112 @@ use App\Util\MensagemErro;
 
 use \PDOException;
 
-class ClubeController {
+class VeiculoController
+{
 
 	private $veiculoDao;
 	private $veiculoMapper;
 	private $veiculoService;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->veiculoDao = new VeiculoDAO();
 		$this->veiculoMapper = new VeiculoMapper();
 		$this->veiculoService = new VeiculoService();
 	}
 
-    public function listar(Request $request, Response $response, 
-							array $args): Response {
-		$clubes = $this->veiculoDao->list();
+	public function listar(
+		Request $request,
+		Response $response,
+		array $args
+	): Response {
+		$veiculos = $this->veiculoDao->list();
 
-		$json = json_encode($clubes, 
-					JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+		$json = json_encode(
+			$veiculos,
+			JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+		);
 
-		$response->getBody()->write($json); 
+		$response->getBody()->write($json);
 		return $response
-				->withStatus(200) //OK
-				->withHeader("Content-Type", "application/json"); 
-    }
+			->withStatus(200) //OK
+			->withHeader("Content-Type", "application/json");
+	}
 
-	public function buscarPorId(Request $request, 
-				Response $response, array $args): Response {
+	public function buscarPorId(
+		Request $request,
+		Response $response,
+		array $args
+	): Response {
 		$id = $args["id"];
 		$veiculo = $this->veiculoDao->findById($id);
 
-		if($veiculo) {
-			$json = json_encode($veiculo, 
-						JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+		if ($veiculo) {
+			$json = json_encode(
+				$veiculo,
+				JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+			);
 
 			$response->getBody()->write($json);
 
 			return $response
-					->withHeader("Content-Type", "application/json")
-					->withStatus(200); //OK
+				->withHeader("Content-Type", "application/json")
+				->withStatus(200); //OK
 		}
 
 		return $response->withStatus(404); //NOT FOUND
-    }
+	}
 
-	public function inserir(Request $request, 
-				Response $response, array $args): Response {
+	public function inserir(
+		Request $request,
+		Response $response,
+		array $args
+	): Response {
 		//Retorna o JSON da requisição no formato de array assoc
 		$jsonArrayAssoc = $request->getParsedBody();
-		$veiculo = 
+		$veiculo =
 			$this->veiculoMapper->mapFromJsonToObject($jsonArrayAssoc);
 
 		//TODO - Validar os dados
 		$erro = $this->veiculoService->validar($veiculo);
-		if($erro){
+		if ($erro) {
 			$jsonErro = MensagemErro::getJSONErro($erro, "", 400);
 			$response->getBody()->write($jsonErro);
 			return $response
-			->withHeader("Content-type", "application/json")
-			->withStatus(400); //BAD REQUEST
+				->withHeader("Content-type", "application/json")
+				->withStatus(400); //BAD REQUEST
 		};
 
 		//Insere o registro do veiculo na base de dados
 		try {
 			$veiculo = $this->veiculoDao->insert($veiculo);
-			$json = json_encode($veiculo, 
-							JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+			$json = json_encode(
+				$veiculo,
+				JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+			);
 
 			$response->getBody()->write($json);
 			return $response
-					->withHeader("Content-Type", "application/json")
-					->withStatus(201); //CREATED
-		
-		
-		} catch(PDOException $ex) {
+				->withHeader("Content-Type", "application/json")
+				->withStatus(201); //CREATED
+
+
+		} catch (PDOException $ex) {
 			//Em caso de erro ao inserir o veiculo, este bloco será executado
 			$jsonErro = MensagemErro::getJSONErro("Erro ao inserir o veiculo!", $ex->getMessage());
 			$response->getBody()->write($jsonErro);
 			return $response
-					->withHeader('Content-Type', 'application/json')
-					->withStatus(500); //INTERNAL_SERVER_ERROR
+				->withHeader('Content-Type', 'application/json')
+				->withStatus(500); //INTERNAL_SERVER_ERROR
 		}
 	}
 
-	public function atualizar(Request $request, Response $response, array $args): Response {
+	public function atualizar(Request $request, Response $response, array $args): Response
+	{
 		$id = $args['id'];
 		$veiculo = $this->veiculoDao->findById($id);
-		
-		if($veiculo) { 
+
+		if ($veiculo) {
 			//Carrega o veiculo que veio na requisição em formato JSON
 			$clubeArrayAssoc = $request->getParsedBody(); //Retorna um array a partir do JSON
 			$veiculo = $this->veiculoMapper->mapFromJsonToObject($clubeArrayAssoc);
@@ -107,58 +125,62 @@ class ClubeController {
 
 			//TODO - Validar os dados
 			$erro = $this->veiculoService->validar($veiculo);
-		if($erro){
-			$jsonErro = MensagemErro::getJSONErro($erro, "", 400);
-			$response->getBody()->write($jsonErro);
-			return $response
-			->withHeader("Content-type", "application/json")
-			->withStatus(400); //BAD REQUEST
-		};
-			
+			if ($erro) {
+				$jsonErro = MensagemErro::getJSONErro($erro, "", 400);
+				$response->getBody()->write($jsonErro);
+				return $response
+					->withHeader("Content-type", "application/json")
+					->withStatus(400); //BAD REQUEST
+			};
+
 			try {
 				//Atualiza o veiculo no banco de dados
-				$veiculo = $this->veiculoDao->update($veiculo);		
+				$veiculo = $this->veiculoDao->update($veiculo);
 
-				$response->getBody()->write(json_encode($veiculo, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE));
+				$response->getBody()->write(json_encode($veiculo, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 				return $response
-						->withHeader('Content-Type', 'application/json')
-						->withStatus(200); //OK
-			
-			} catch(PDOException $ex) {
+					->withHeader('Content-Type', 'application/json')
+					->withStatus(200); //OK
+
+			} catch (PDOException $ex) {
 				//Em caso de erro ao atualizar o veiculo, este bloco será executado
 				$jsonErro = MensagemErro::getJSONErro("Erro ao atualizar o veiculo!", $ex->getMessage());
 				$response->getBody()->write($jsonErro);
 				return $response
-						->withHeader('Content-Type', 'application/json')
-						->withStatus(500); //INTERNAL_SERVER_ERROR
+					->withHeader('Content-Type', 'application/json')
+					->withStatus(500); //INTERNAL_SERVER_ERROR
 			}
 		}
 
 		return $response->withStatus(404); //NOT_FOUND
-    }
-
-	public function deletar(Request $request, Response $response, array $args): Response {
+	}
+	public function deletar(Request $request, Response $response, array $args): Response
+	{
 		$id = $args['id'];
 		$veiculo = $this->veiculoDao->findById($id);
-		
-		if($veiculo) { 
+
+		if ($veiculo) {
 			try {
-				//Deleta do banco de dados
+				// Deleta do banco de dados
 				$this->veiculoDao->deleteById($id);
-				return $response->withStatus(200); //OK
-			} catch(PDOException $ex) {
-				
-				//Em caso de erro ao atualizar o veiculo, este bloco será executado
-				$jsonErro = MensagemErro::getJSONErro("Erro ao deletar o veiculo!", $ex->getMessage());
+
+				// Resposta de sucesso
+				$mensagem = ["mensagem" => "Veículo deletado com sucesso"];
+				$response->getBody()->write(json_encode($mensagem));
+
+				return $response
+					->withHeader('Content-Type', 'application/json')
+					->withStatus(200); // OK
+			} catch (PDOException $ex) {
+				// Em caso de erro ao deletar o veículo, este bloco será executado
+				$jsonErro = MensagemErro::getJSONErro("Erro ao deletar o veículo!", $ex->getMessage());
 				$response->getBody()->write($jsonErro);
 				return $response
-						->withHeader('Content-Type', 'application/json')
-						->withStatus(500); //INTERNAL_SERVER_ERROR
+					->withHeader('Content-Type', 'application/json')
+					->withStatus(500); // INTERNAL_SERVER_ERROR
 			}
 		}
 
-		return $response->withStatus(404); //NOT_FOUND
-    }
-
-
+		return $response->withStatus(404); // NOT_FOUND
+	}
 }
