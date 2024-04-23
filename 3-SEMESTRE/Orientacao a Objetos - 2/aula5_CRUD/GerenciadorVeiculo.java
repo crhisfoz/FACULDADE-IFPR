cfc0308a62cc08260b532d5d1f1652877b9360f5
crhisfoz <crhisnokia@live.com>
2024-04-23 14:51:08 -0300

    refactor: continuacao de exercicio crud em java

--- 3-SEMESTRE/Orientacao a Objetos - 2/aula5_CRUD/GerenciadorVeiculo.java
+++ 3-SEMESTRE/Orientacao a Objetos - 2/aula5_CRUD/GerenciadorVeiculo.java
@@ -1,27 +1,29 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.mysql.cj.protocol.a.NativeConstants.IntegerDataType;
+
 public class GerenciadorVeiculo {
     Scanner scanner;
     DaoVeiculo daoVeiculo;
 
-    public GerenciadorVeiculo() {
+    public GerenciadorVeiculo(){
         scanner = new Scanner(System.in);
         daoVeiculo = new DaoVeiculo();
     }
 
-    public void menu() {
+    public void menu(){
         int opcao = -1;
         while (opcao != 0) {
-            System.out.println("\n-------------------------------");
-            System.out.println("Gerenciamento de Veiculos");
-            System.out.println("[1] Cadastrar");
-            System.out.println("[2] Consultar");
-            System.out.println("[3] Alterar");
-            System.out.println("[4] Excluir");
-            System.out.println("[5] Listar Todos");
-            System.out.println("[0] Sair");
-            System.out.println("\n-------------------------------");
+			System.out.println("\n-------------------------------");
+			System.out.println("Gerenciamento de Veiculos");            
+			System.out.println("[1] Cadastrar");            
+			System.out.println("[2] Consultar");            
+			System.out.println("[3] Alterar");            
+			System.out.println("[4] Excluir");            
+			System.out.println("[5] Listar Todos");            
+			System.out.println("[0] Sair");            
+			System.out.println("\n-------------------------------");
 
             try {
                 opcao = Integer.parseInt(scanner.nextLine());
@@ -30,32 +32,32 @@ public class GerenciadorVeiculo {
             }
 
             switch (opcao) {
-                case 1:
-                    this.cadastrar();
-                    break;
-                case 2:
-                    this.consultar();
-                    break;
-                case 3:
-                    this.alterar();
-                    break;
-                case 4:
-                    this.excluir();
-                    break;
-                case 5:
-                    this.listarTodos();
-                    break;
-                case 0:
-                    System.out.println("Até logo.");
-                    break;
-                default:
-                    System.out.println("Opção inválida.");
-                    break;
+            case 1:
+                this.cadastrar();
+                break;
+            case 2:
+                this.consultar();
+                break;
+            case 3:
+                this.alterar();
+                break;
+            case 4:
+                this.excluir();
+                break;
+            case 5:
+                this.listarTodos();
+                break;
+            case 0:
+                System.out.println("Até logo.");
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                break;
             }
         }
     }
 
-    public void cadastrar() {
+    public void cadastrar(){
         Veiculo v = new Veiculo();
         System.out.println("------------------------------");
         System.out.println("[Cadastro de Veículos]");
@@ -68,74 +70,74 @@ public class GerenciadorVeiculo {
         System.out.println("Ano: ");
         v.setAno(Integer.parseInt(scanner.nextLine()));
 
-        // passa para a camada model
+        //passa para a camada model
         boolean inserido = daoVeiculo.inserir(v);
-        if (inserido) {
+        if(inserido){
             System.out.println("Inserido com sucesso.");
         }
     }
 
-    public void listarTodos() {
+    public void listarTodos(){
         ArrayList<Veiculo> veiculos = daoVeiculo.buscarTodos();
         System.out.println("----------------------------");
         System.out.println("Veículos Cadastrados");
-        for (Veiculo v : veiculos) {
+        for(Veiculo v : veiculos){
             System.out.println("Código: " + v.getCodigo()
-                    + ", Marca: " + v.getMarca()
-                    + ", Modelo: " + v.getModelo()
-                    + ", Chassi: " + v.getChassi()
-                    + ", Ano: " + v.getAno());
+                + ", Marca: " + v.getMarca()
+                + ", Modelo: " + v.getModelo()
+                + ", Chassi: " + v.getChassi()
+                + ", Ano: " + v.getAno());
         }
     }
 
-    public void excluir() {
-        System.out.println("\n ----------------- \n");
-        System.out.println("Exclusão de veículo \n");
-        System.out.println("[Código]");
+    public void excluir(){
+        System.out.println("----------------------------");
+        System.out.println("Exclusão de veículo");
+        System.out.println("Código:");
         int codigo = Integer.parseInt(scanner.nextLine());
         int qtde = daoVeiculo.excluir(codigo);
-        if (qtde > 0) {
-
-            System.out.println("Excluído com sucesso");
-        } else {
-            System.out.println("Não há dados para Excluir");
-
+        if(qtde > 0){
+            System.out.println("Excluído com sucesso.");
+        }else{
+            System.out.println("Não encontrado...");
         }
     }
 
-    public void consultar() {
+    public void consultar(){
         System.out.println("----------------------------");
-        System.out.println("Consulta de Veículo");
-        System.out.println("Código: ");
+        System.out.println("Consulta de veículo");
+        System.out.println("Código:");
         int codigo = Integer.parseInt(scanner.nextLine());
         Veiculo v = daoVeiculo.consultar(codigo);
-        if (v != null) {
+        if(v != null){
             System.out.println("[Dados do veículo]");
-            System.out.println("Código: " + v.getCodigo()
-                    + ", Marca: " + v.getMarca()
-                    + ", Modelo: " + v.getModelo()
-                    + ", Chassi: " + v.getChassi()
-                    + ", Ano: " + v.getAno());
-        } else {
+            System.out.println("Código: " + v.getCodigo());
+            System.out.println("Marca: " + v.getMarca());
+            System.out.println("Modelo: " + v.getModelo());
+            System.out.println("Chassi: " + v.getChassi());
+            System.out.println("Ano: " + v.getAno());
+        }else{
             System.out.println("Não encontrado...");
         }
     }
 
     public void alterar(){
-        System.out.println("\n --------------- \n");
+        System.out.println("----------------------------");
         System.out.println("Alteração de veículo");
-        System.out.println("Código: " );
+        System.out.println("Código:");
         int codigo = Integer.parseInt(scanner.nextLine());
         Veiculo v = daoVeiculo.consultar(codigo);
+
         if(v != null){
-            //mostrar cada campo e ler um novo dado.
-            System.out.println("\n\n ----- VEÍCULO CONSULTADO -----\n");
+            System.out.println("[Dados do veículo]");
             System.out.println("[Código: " + v.getCodigo() + "]");
-            System.out.println("[Código: " + v.getMarca() + "]");
+
+            System.out.println("[Marca: " + v.getMarca() + "]");
             String marca = scanner.nextLine();
             if(!marca.isEmpty()){
                 v.setMarca(marca);
             }
+
             System.out.println("[Modelo: " + v.getModelo() + "]");
             String modelo = scanner.nextLine();
             if(!modelo.isEmpty()){
@@ -151,21 +153,17 @@ public class GerenciadorVeiculo {
             System.out.println("[Ano: " + v.getAno() + "]");
             String ano = scanner.nextLine();
             if(!ano.isEmpty()){
-               v.setAno(Integer.parseInt(ano));
+                v.setAno(Integer.parseInt(ano));
             }
 
-            int qtdeAlteracao = daoVeiculo.alterar(v);
-            if(qtdeAlteracao > 0){
-                System.out.println("Atualizado com sucesso");
+            int qtdeAlterado = daoVeiculo.alterar(v);
+            if(qtdeAlterado > 0){
+                System.out.println("Atualizado com sucesso.");
             }else{
-                System.out.println("Não possível atualizar o registro");
+                System.out.println("Não foi possível atualizar.");
             }
         }else{
-            System.out.println("Veículo não encontrado");
-        }
-        ;
-
-        
-
+            System.out.println("Não encontrado...");
+        }        
     }
 }
---
+++ 3-SEMESTRE/Orientacao a Objetos - 2/aula5_CRUD/Salesman.java
@@ -0,0 +1,45 @@
+public class Salesman{
+    private int id;
+    private String name;
+    private int cpf;
+    private int phone;
+    private float salary;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public int getPhone() {
+        return phone;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+    public float getSalary() {
+        return salary;
+    }
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+
+
+
+    
+    
+}
